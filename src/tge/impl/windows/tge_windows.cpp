d58496e9f5000ef75dd9d9469c16408e31d51afd
Harrand <harrysjh98@gmail.com>
2023-01-06 01:22:44 +0000

    * Windows: WNDCLASSEXA registration

--- src/tge/impl/windows/tge_windows.cpp
+++ src/tge/impl/windows/tge_windows.cpp
@@ -1,17 +1,39 @@
 #include "tge/impl/windows/tge_windows.hpp"
 #ifdef _WIN32
-#include "tge/impl/windows/detail/winapi.hpp"
 
 namespace tge::impl
 {
+	constexpr WNDCLASSEXA wndclass_gpuacc
+	{
+		.cbSize = sizeof(WNDCLASSEX),
+		.style = CS_OWNDC,
+		.lpfnWndProc = wndproc,
+		.cbClsExtra = 0,
+		.cbWndExtra = 0,
+		.hInstance = nullptr,
+		.hIcon = nullptr,
+		.hCursor = nullptr,
+		.hbrBackground = nullptr,
+		.lpszMenuName = nullptr,
+		.lpszClassName = "Tangle Windowing Library",
+		.hIconSm = nullptr
+	};
+
 	void initialise_windows()
 	{
-
+		auto window_class = wndclass_gpuacc;
+		window_class.hInstance = GetModuleHandle(nullptr);
+		RegisterClassExA(&window_class);
 	}
 
 	void terminate_windows()
 	{
+		UnregisterClassA(wndclass_gpuacc.lpszClassName, GetModuleHandle(nullptr));
+	}
 
+	LRESULT wndproc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+	{
+		return DefWindowProc(hwnd, msg, wparam, lparam);
 	}
 }
 
--- src/tge/impl/windows/tge_windows.hpp
+++ src/tge/impl/windows/tge_windows.hpp
@@ -1,9 +1,11 @@
 #ifndef TANGLE_IMPL_WINDOWS_TGE_HPP
 #define TANGLE_IMPL_WINDOWS_TGE_HPP
 #ifdef _WIN32
+#include "tge/impl/windows/detail/winapi.hpp"
 
 namespace tge::impl
 {
+	LRESULT wndproc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 	void initialise_windows();
 	void terminate_windows();
 }
