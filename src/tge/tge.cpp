9a103221b28641c2bbfd980e9c42f83a6d635718
Harrand <harrysjh98@gmail.com>
2023-01-06 00:29:10 +0000

    + Added tge::get_platform()

--- src/tge/tge.cpp
+++ src/tge/tge.cpp
@@ -1,14 +1,15 @@
 #include "tge/tge.hpp"
+#include "hdk/hdk.hpp"
 
 namespace tge
 {
 	void initialise()
 	{
-
+		hdk::initialise();
 	}
 	
 	void terminate()
 	{
-
+		hdk::terminate();
 	}
 }
--- src/tge/tge.hpp
+++ src/tge/tge.hpp
@@ -5,6 +5,31 @@ namespace tge
 {
 	void initialise();
 	void terminate();
+
+	enum class platform
+	{
+		unknown,
+		windows,
+		linux,
+		macos
+	};
+
+	constexpr platform get_platform()
+	{
+		#ifdef _WIN32
+			return platform::windows;
+		#else
+			#ifdef __linux__
+				return platform::linux;
+			#else
+				#ifdef __APPLE__
+					return platform::macos;
+				#else
+					return platform::unknown;
+				#endif
+			#endif
+		#endif
+	}
 }
 
 #endif // TANGLE_TGE_HPP
