a38d51d40e18770c4a3dae51b7bac87a6b1ba4eb
Harrand <harrysjh98@gmail.com>
2023-01-06 01:12:21 +0000

    + Added impl/windows/detail/winapi.hpp to include winapi properly

--- src/tge/impl/windows/tge_windows.cpp
+++ src/tge/impl/windows/tge_windows.cpp
@@ -1,10 +1,12 @@
 #include "tge/impl/windows/tge_windows.hpp"
 #ifdef _WIN32
+#include "tge/impl/windows/detail/winapi.hpp"
 
 namespace tge::impl
 {
 	void initialise_windows()
 	{
+
 	}
 
 	void terminate_windows()
